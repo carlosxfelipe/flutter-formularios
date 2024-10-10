c2b43eee40e200a3fbd94290843c8a4bf6543b0e
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-10-09 21:33:41 -0300

    screens

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,5 +5,9 @@ final appRouter = GoRouter(routes: [
   GoRoute(
     path: '/',
     builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
+    path: '/cubits',
+    builder: (context, state) => const CubitCounterScreen(),
   )
 ]);
