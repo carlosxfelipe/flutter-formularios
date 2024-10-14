69995bd253ae4424b19a0c3bdeec0826d83c9d91
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-10-13 21:53:32 -0300

    divider

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -20,6 +20,10 @@ class HomeScreen extends StatelessWidget {
             subtitle: const Text("Gerenciamento de estado previsível"),
             trailing: const Icon(Icons.arrow_forward_ios_rounded),
             onTap: () => context.push('/bloc'),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Divider(),
           )
         ],
       ),
